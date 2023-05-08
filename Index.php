78da1820c1475ab85c1fcb48f1de028ada2a958a
Keven Nogueira <kevennogueira161@gmail.com>
2023-05-08 14:13:16 -0300

    Alteração de margin

--- Index.php
+++ Index.php
@@ -12,7 +12,8 @@ require 'logica/TarefaController.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
 
     <!-- Fontewesom -->
     <script src="https://kit.fontawesome.com/7d3a8355c9.js" crossorigin="anonymous"></script>
@@ -52,17 +53,20 @@ require 'logica/TarefaController.php';
                             </h4>
 
                             <?php foreach ($tarefas as $indice => $tarefa) { ?>
-                                <div class="row mb-3 d-flex align-items-center tarefa">
-                                    <div class="col-sm-9" id="tarefa_<?php echo $tarefa->id ?>">
-                                        <?php echo $tarefa->dsc_tarefa ?>
-                                    </div>
-                                    <div class="col-sm-3 mt-2 d-flex justify-content-between">
-                                        <i class="fas fa-trash-alt fa-lg text-danger" title="Excluir" onclick="remover(<?php echo $tarefa->id ?>, 'index')"></i>
-                                        <i class="fas fa-edit fa-lg text-info" title="Editar" onclick="editar(<?php echo $tarefa->id ?>, '<?php echo $tarefa->dsc_tarefa ?>', 'index')"></i>
-                                        <i class="fas fa-check-square fa-lg text-success" title="Marcar como Realizada" onclick="marcarRealizada(<?php echo $tarefa->id ?>, 'index')"></i>
-                                    </div>
+                            <div class="row mb-3 d-flex align-items-center tarefa">
+                                <div class="col-sm-9" id="tarefa_<?php echo $tarefa->id ?>">
+                                    <?php echo $tarefa->dsc_tarefa ?>
                                 </div>
-                                <hr>
+                                <div class="col-sm-3 mt-3 mb-2 d-flex justify-content-between">
+                                    <i class="fas fa-trash-alt fa-lg text-danger" title="Excluir"
+                                        onclick="remover(<?php echo $tarefa->id ?>, 'index')"></i>
+                                    <i class="fas fa-edit fa-lg text-info" title="Editar"
+                                        onclick="editar(<?php echo $tarefa->id ?>, '<?php echo $tarefa->dsc_tarefa ?>', 'index')"></i>
+                                    <i class="fas fa-check-square fa-lg text-success" title="Marcar como Realizada"
+                                        onclick="marcarRealizada(<?php echo $tarefa->id ?>, 'index')"></i>
+                                </div>
+                            </div>
+                            <hr>
                             <?php } ?>
                         </div>
                     </div>
@@ -72,11 +76,14 @@ require 'logica/TarefaController.php';
     </div>
     <!-- JavaScript (Opcional) -->
     <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script>
 
     <!-- Incio do JS personalizado -->
--- TodasTarefas.php
+++ TodasTarefas.php
@@ -12,7 +12,8 @@ require 'logica/TarefaController.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
 
     <!-- Fontewesom -->
     <script src="https://kit.fontawesome.com/7d3a8355c9.js" crossorigin="anonymous"></script>
@@ -50,21 +51,24 @@ require 'logica/TarefaController.php';
                                 Tarefas</h4>
 
                             <?php foreach ($tarefas as $indice => $tarefa) { ?>
-                                <div class="row mb-3 d-flex align-items-center tarefa">
-                                    <div class="col-sm-9" id="tarefa_<?php echo $tarefa->id ?>">
-                                        <?php echo $tarefa->dsc_tarefa ?> (<?php echo $tarefa->status ?>)
-                                    </div>
-                                    <div class="col-sm-3 mt-2 d-flex justify-content-between">
-                                        <i class="fas fa-trash-alt fa-lg text-danger" title="Excluir" onclick="remover(<?php echo $tarefa->id ?>)"></i>
-
-                                        <?php if ($tarefa->status == 'pendente') { ?>
-                                            <i class="fas fa-edit fa-lg text-info" title="Editar" onclick="editar(<?php echo $tarefa->id ?>, '<?php echo $tarefa->dsc_tarefa ?>')"></i>
-                                            <i class="fas fa-check-square fa-lg text-success" title="Marcar como Realizada" onclick="marcarRealizada(<?php echo $tarefa->id ?>)"></i>
-                                        <?php } ?>
-
-                                    </div>
+                            <div class="row mb-3 d-flex align-items-center tarefa">
+                                <div class="col-sm-9" id="tarefa_<?php echo $tarefa->id ?>">
+                                    <?php echo $tarefa->dsc_tarefa ?> (<?php echo $tarefa->status ?>)
                                 </div>
-                                <hr>
+                                <div class="col-sm-3 mt-3 mb-2 d-flex justify-content-between">
+                                    <i class="fas fa-trash-alt fa-lg text-danger" title="Excluir"
+                                        onclick="remover(<?php echo $tarefa->id ?>)"></i>
+
+                                    <?php if ($tarefa->status == 'pendente') { ?>
+                                    <i class="fas fa-edit fa-lg text-info" title="Editar"
+                                        onclick="editar(<?php echo $tarefa->id ?>, '<?php echo $tarefa->dsc_tarefa ?>')"></i>
+                                    <i class="fas fa-check-square fa-lg text-success" title="Marcar como Realizada"
+                                        onclick="marcarRealizada(<?php echo $tarefa->id ?>)"></i>
+                                    <?php } ?>
+
+                                </div>
+                            </div>
+                            <hr>
                             <?php } ?>
                         </div>
                     </div>
@@ -74,11 +78,14 @@ require 'logica/TarefaController.php';
     </div>
     <!-- JavaScript (Opcional) -->
     <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script>
 
     <!-- Incio do JS personalizado -->
